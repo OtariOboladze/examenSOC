d19611355c4d2fdfbc06466920d72e18da87d1dc
OtariOboladze <otari.oboladze@outlook.com>
2020-12-15 10:35:31 +0100

    PARTE 2, 2 combo caja derecha

--- main.js
+++ main.js
@@ -2,50 +2,46 @@
 document.getElementById("submit").addEventListener("click", display_input);
 
 function checkbox() {
-    let x = "";
-    for (let i = 1; i < 5; i++) {
-        if (document.getElementById(i).checked == true) {
-            x += i + " ";
-        }
+  let x = "";
+  for (let i = 1; i < 5; i++) {
+    if (document.getElementById(i).checked == true) {
+      x += i + " ";
     }
-    return (x);
+  }
+  return x;
 }
 
 function display_input() {
-    //utilizaremos regex para comprobar el tipo de input
-    var any_number = /\d/;
-    var only_numbers = /^[0-9]+$/;
-    document.getElementById("texto").style.backgroundColor = "white";
-    document.getElementById("numeros").style.backgroundColor = "white";
-    if (document.getElementById("texto").value.match(any_number)) {
-        document.getElementById("texto").style.backgroundColor = "yellow";
-        alert("formato de texto no es correcto, se admite solo las letras");
-    } else if (!(document.getElementById("numeros").value.match(only_numbers))) {
-        document.getElementById("numeros").style.backgroundColor = "yellow";
-        alert("formato de numeros no es correcto, se admite solo los numeros")
-    } else {
-        document.getElementById("caja_amarillo_inner_text").innerText = document.getElementById("texto").value + " / " + document.getElementById("numeros").value + " / " + checkbox();
-    }
+  //utilizaremos regex para comprobar el tipo de input
+  var any_number = /\d/;
+  var only_numbers = /^[0-9]+$/;
+  document.getElementById("texto").style.backgroundColor = "white";
+  document.getElementById("numeros").style.backgroundColor = "white";
+  if (document.getElementById("texto").value.match(any_number)) {
+    document.getElementById("texto").style.backgroundColor = "yellow";
+    alert("formato de texto no es correcto, se admite solo las letras");
+  } else if (!document.getElementById("numeros").value.match(only_numbers)) {
+    document.getElementById("numeros").style.backgroundColor = "yellow";
+    alert("formato de numeros no es correcto, se admite solo los numeros");
+  } else {
+    document.getElementById("caja_amarillo_inner_text").innerText =
+      document.getElementById("texto").value +
+      " / " +
+      document.getElementById("numeros").value +
+      " / " +
+      checkbox();
+  }
 }
 //PARTE IZQUIERDA end  ////////////////////////////////////////////////////////////////////////////
 
 //PARTE DERECHA start   //////////////////////////////////////////////////////////////////////////
 
-document.getElementById("colores").onchange("colores", backgounrd_color);
+document.getElementById("colores").addEventListener("change", background_color);
 
-
-function backgounrd_color() {
-    var x = document.getElementById("colores");
-    for (let i = 0; i < x.length; i++) {
-        var e = x[i];
-        console.log(e);
-        // if (document.getElementById(e).selected == true) {
-        //     document.getElementById("right").style.backgroundColor = e;
-        // }
-    }
+function background_color() {
+  document.getElementById(
+    "right"
+  ).style.backgroundColor = document.getElementById("colores").value;
 }
 
-backgounrd_color();
-
 //PARTE DERECHA end    //////////////////////////////////////////////////////////////////////////
-
